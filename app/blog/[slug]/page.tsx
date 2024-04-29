2d3324a2ad91322f0dbd7fb8aee006bd33cf85bd
githiyon <githiyonkowshik@gmail.com>
2024-04-29 17:33:44 +0530

    add blog page

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import Blog from "../page";
+
+export const metadata: Metadata ={
+    title:'Blog|keedriver',
+    description:'This is Blog page for keedriver Next.js'
+}
+
+
+async function getData(slug : any) {
+    const cookieStore = cookies();
+    const access = cookieStore.get("access");
+
+    const res = await fetch('url',{
+        headers:{
+            "Content-Type" : "application/json",
+            Authorization:`Bearer ${access?.value}`,
+        },
+    });
+
+    if(!res.ok){
+        redirect('/login')
+    }
+    const blog = await res.json();
+    return blog;
+}
+
+const BlogPage = async({
+    params :{slug},
+}:{
+    params : {slug:string};
+}) =>{
+    // const data:Data = await getData(slug);
+
+    return <>
+    <Blog/>
+
+    </>
+}
