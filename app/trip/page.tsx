30fe8c19ddaa23b4d35b289bae5bd8d247484a05
githiyon <githiyonkowshik@gmail.com>
2024-05-15 12:00:11 +0530

    trip form design completed

--- app/trip/page.tsx
+++ app/trip/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import TripForm from './trip-form'
+import { TripDetailForm,PickupLocation } from './pickup-location'
+import { Input } from '@/components/ui/input'
 
 
 export const metadata:Metadata =  {
@@ -10,9 +12,9 @@ export const metadata:Metadata =  {
 
 const TripPage = () => {
   return (
-    <div>
-        <TripForm
-        />
+    <div className='h-[80vh] flex flex-col items-center'>
+        <TripDetailForm/>
+        {/* <PickupLocation/> */}
 
         
     </div>
